bac889a59962b2c62be6b3846cd2dc5abd826c25
James Harris <computersherpa@gmail.com>
2017-09-28 17:25:24 -0500

    View (imperfect) globe model

--- js/globe.js
+++ js/globe.js
@@ -5,18 +5,38 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+var loader = new THREE.OBJLoader(), globe;
+loader.load('assets/globe-countries.obj', function(obj) {
+	globe = obj;
 
-camera.position.z = 5;
+	globe.position.y = -40;
+	globe.position.x = 20;
+	globe.scale.x = globe.scale.y = globe.scale.z = 5;
+
+	scene.add(globe);
+});
+
+var light = new THREE.AmbientLight( 0x404040 ); // soft white light
+scene.add(light);
+scene.background = new THREE.Color(0x72645b);
+
+var plane = new THREE.Mesh(
+	new THREE.PlaneBufferGeometry( 40, 40 ),
+	new THREE.MeshPhongMaterial( { color: 0x999999, specular: 0x101010 } )
+);
+plane.rotation.x = -Math.PI/2;
+plane.position.y = -0.5;
+scene.add( plane );
+
+plane.receiveShadow = true;
+
+camera.position.y = 5
+camera.position.z = 25
 
 function animate() {
 	requestAnimationFrame(animate);
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.1;
+	// if (globe) globe.rotation.x += .01;
 
 	renderer.render(scene, camera);
 }
