b9c5e7df831ee6392e3cc97b1dd117b76b44adea
James Harris <computersherpa@gmail.com>
2017-09-29 16:22:27 -0500

    LatLong to screen coords

--- js/globe.js
+++ js/globe.js
@@ -3,30 +3,35 @@ function sceneSetup() {
 	scene = new THREE.Scene();
 	camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 	renderer = new THREE.WebGLRenderer();
-		renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.setSize(window.innerWidth, window.innerHeight);
 	document.body.appendChild(renderer.domElement);
-	
+
 	var loader = new THREE.OBMLoader();
 	loader.load('assets/globes_pack_thin26small.obm', function(obj) {
 		globe = obj;
-	
+
 		// var mat = new THREE.MeshPhongMaterial( { color: 0x999999, specular: 0x101010 } );
-	
+
 		console.log(globe);
 		// globe.traverse(function(child) {
 		// 	child.material = mat;
 		// })
 		// globe.children[1].material = mat;
-	
+
 		scene.add(globe);
-	})
-	
-	var light = new THREE.PointLight( 0x404040 ); // soft white light
+
+		var box = new THREE.Box3().setFromObject(globe);
+		globe.radius = box.max;
+
+		test();
+	});
+
+	var light = new THREE.PointLight(0x404040); // soft white light
 	light.position.y = 5;
 	light.position.x = -5;
 	scene.add(light);
 	scene.background = new THREE.Color(0x72645b);
-	
+
 	camera.position.y = 1;
 	camera.position.z = 3;
 	camera.rotation.x = -0.4;
@@ -36,16 +41,31 @@ sceneSetup();
 function animate() {
 	requestAnimationFrame(animate);
 
-	if (globe) globe.rotation.y += .001;
+	// if (globe) globe.rotation.y += .001;
 
 	renderer.render(scene, camera);
 }
 animate();
 
-function sceneToScreenCoords(x, y, z) {
+function latLongToSceneCoords(lat, lon) {
+	var out = new THREE.Vector3();
+	var radius = globe.radius.y;
+	
+	var phi   = (90-lat)*(Math.PI/180)
+	var theta = (lon+180)*(Math.PI/180)
+
+	out.x = -((radius) * Math.sin(phi)*Math.cos(theta))
+	out.z = ((radius) * Math.sin(phi)*Math.sin(theta))
+	out.y = ((radius) * Math.cos(phi))
+
+	return out;
+}
+
+function sceneToScreenCoords(sceneCoords) {
+	// adapted from https://stackoverflow.com/q/10473852/1805453
 	var vector = new THREE.Vector3();
 	var canvas = renderer.domElement;
-	vector.set(x, y, z);
+	vector.set(sceneCoords.x, sceneCoords.y, sceneCoords.z);
 
 	vector.project(camera);
 
@@ -55,10 +75,16 @@ function sceneToScreenCoords(x, y, z) {
 
 	return vector;
 }
-var $center = $('<img src="assets/marker.svg" alt="" class="marker" />');
-var coords = sceneToScreenCoords(0,0,0);
-$center.css({
-	top: coords.y,
-	left: coords.x
-});
-$center.appendTo('body');
+
+function test() {
+	var $southPole = $('<img src="assets/marker.svg" alt="" class="marker" />');
+	var sceneCoords = latLongToSceneCoords(32.777663, -96.630416);
+	var screenCoords = sceneToScreenCoords(sceneCoords);
+	console.log("sceneCoords:", sceneCoords, "screenCoords", screenCoords);
+	$southPole.css({
+		top: screenCoords.y,
+		left: screenCoords.x
+	});
+	var $body = $('body');
+	$body.append($southPole);
+}
