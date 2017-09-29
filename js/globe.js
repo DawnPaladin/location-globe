4c7eba7df6129d286dc49aae21335e7fda6a5ac2
James Harris <computersherpa@gmail.com>
2017-09-29 11:37:33 -0500

    Change wireframe globe to mesh

--- js/globe.js
+++ js/globe.js
@@ -5,60 +5,35 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var loader = new THREE.OBJLoader(), land, sea;
-loader.load('assets/globe-countries.obj', function(obj) {
-	land = obj;
-
-	land.position.y = -40;
-	land.position.x = 20;
-	land.scale.x = land.scale.y = land.scale.z = 5;
-	console.log(land);
-
-	// scene.add(land);
-});
-loader.load('assets/wire-globe.obj', function(obj) {
-	sea = obj;
-
-	sea.scale.x = sea.scale.y = sea.scale.z = 3;
-	sea.position.y = -25;
-
-	var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-	sea.traverse(function(child) {
-		if (child instanceof THREE.Mesh) {
-			child.material = material;
-		}
-	})
-
-	// scene.add(sea);
-})
-var globe;
-loader.load('assets/globes_pack7.obj', function(obj) {
+var loader = new THREE.OBJLoader(), globe;
+loader.load('assets/globes_pack13.obj', function(obj) {
 	globe = obj;
+
+	// var mat = new THREE.MeshPhongMaterial( { color: 0x999999, specular: 0x101010 } );
+
+	console.log(globe);
+	// globe.traverse(function(child) {
+	// 	child.material = mat;
+	// })
+	// globe.children[1].material = mat;
 
 	scene.add(globe);
 })
 
-var light = new THREE.AmbientLight( 0x404040 ); // soft white light
+var light = new THREE.PointLight( 0x404040 ); // soft white light
+light.position.y = 5;
+light.position.x = -5;
 scene.add(light);
 scene.background = new THREE.Color(0x72645b);
 
-var plane = new THREE.Mesh(
-	new THREE.PlaneBufferGeometry( 40, 40 ),
-	new THREE.MeshPhongMaterial( { color: 0x999999, specular: 0x101010 } )
-);
-plane.rotation.x = -Math.PI/2;
-plane.position.y = -0.5;
-scene.add( plane );
-
-plane.receiveShadow = true;
-
-// camera.position.y = 12;
-camera.position.z = 5;
+camera.position.y = 1;
+camera.position.z = 2.5;
+camera.rotation.x = -0.4;
 
 function animate() {
 	requestAnimationFrame(animate);
 
-	// if (globe) globe.rotation.x += .01;
+	if (globe) globe.rotation.y += .001;
 
 	renderer.render(scene, camera);
 }
