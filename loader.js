d67af43d12a67f36b009ffb2c99f53cd3959ab04
James Harris <computersherpa@gmail.com>
2018-05-23 17:59:51 -0500

    Add loadScript function, switch from promises to $.when, async flag

--- loader.js
+++ loader.js
@@ -1,28 +1,39 @@
+// loader.js
+
 /**
  * Loads all the scripts passed to it, returning a promise that they've all loaded.
  * @param {string[]} urls: An array of URLs to load
- * @param {boolean} debug: Enable to print a console message when a script starts and finishes loading
+ * @param {boolean} async Load asynchronously
+ * @param {boolean} debug Enable to print a console message when a script starts and finishes loading
  * @returns {promise} A promise that all the URLs have loaded
  */
-function loadScripts(urls, debug) {
-	function addScriptToPromises(promises, url) {
-		if (debug) console.log("Adding " + url + " to promises");
-		promises.push($.ajax({
+function loadScripts(urls, async, debug) {
+	var promises = urls.map(function(url) {
+		if (debug) console.log("Loading", url, "with async = ", async);
+		return $.ajax({
 			url: url,
 			dataType: "script",
 			cache: true,
+			async: async,
 			error: function(jqXHR, textStatus, errorThrown) {
 				console.error("Couldn't load script: ", url, textStatus, errorThrown, jqXHR);
 			},
 			complete: function() {
 				if (debug) console.log("Loaded", url);
 			}
-		}));
-	}
-	
-	var promises = [];
-	urls.forEach(function(url) { addScriptToPromises(promises, url); });
-	return Promise.all(promises);
+		});
+	});
+	return $.when(promises);
+}
+/**
+ * 
+ * @param {string} url A URL to load
+ * @param {boolean} async Load asynchronously
+ * @param {boolean} debug Enable to print a console message when a script starts and finishes loading
+ * @returns {promise} A promise that the URL has loaded
+ */
+function loadScript(url, async, debug) {
+	return loadScripts([url], async, debug);
 }
 
 ///
@@ -34,10 +45,10 @@ loadScripts([
 	"/hki-sitefinity-theme/news-globe/vendor/two.js",
 	"/hki-sitefinity-theme/news-globe/vendor/three.js",
 	"/hki-sitefinity-theme/news-globe/facilities.js",
-]).then(function() {
-	return loadScripts(["/hki-sitefinity-theme/news-globe/vendor/OBJLoader.js"]);
-}).then(function() {
-	return loadScripts(["/hki-sitefinity-theme/news-globe/vendor/OBMLoader.min.js"]);
-}).then(function() {
-	return loadScripts(["/hki-sitefinity-theme/news-globe/globe.js"]);
+], false, true).done(function() {
+	return loadScript("/hki-sitefinity-theme/news-globe/vendor/OBJLoader.js", false, true);
+}).done(function() {
+	return loadScript("/hki-sitefinity-theme/news-globe/vendor/OBMLoader.min.js", false, true);
+}).done(function() {
+	return loadScript("/hki-sitefinity-theme/news-globe/globe.js", true, true);
 });
